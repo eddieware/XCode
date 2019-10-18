93ac44d4e4ebbd7c9874e2bbb69cbc0a33071141
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2019-10-18 10:33:22 -0500

    Ejercicios de swift

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+var message = "Hello, this is a new kind of concatenation"
+print ("me acaban de saludar con unca conca nueva: \(message)")
+
+let pi = 3.141692
+let minT = -273.15
+
+let meaningOfLife = 42
+let anotherpi = 3 + 0.141592
+
+//Literasles numericos
+// Decimal
+let decimalNumber = 17
+//Binario
+let decimalBinario = 0b10001
+//Octal
+let decimalOctal = 0o21
+let decimalHexa = 0x11
+let number = 1.25e2
+let oneMillion = 1_000_000
+
+//Casting
+// constante nombre : designacion de bits = valor
+let twoThousand : UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndone = twoThousand + UInt16(one)
+//Comando para saber el tipo de dato en swift
+type(of: twoThousand)
+
+let three = Double(3)
+type(of: three)
+
+let oiFloat = Float(3.141592265)
+let intPi = Int(3.141592265)
+//Si la precision importa mucho mejor usa un double ya que como se puede observar el Float solo toma 6 decimales
+let newN = -5.6565
+let doublePi = Double(three) + 0.14159265
+let newnumber = Int16.max
+print(newnumber)
+
+//Boolean Data
+var myBool: Bool
+let orangeAreOrage = false
+let MangosCool = false
+
+if orangeAreOrage {
+    print("Me gustan las naranjas")
+}else{
+    print("No me gustan las naranjas")
+}
+
+let i=3
+
+let myComparison = (i==3)
+
+if i == 3 {
+    print(tengo un 3)
+}else{
+    
+    print("No tengo un 3")
+}
---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let maxconst = 10
+var cla = 0
+
