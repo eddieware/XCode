9135ac32c6c2f73757272e3ceefeaa8aba265fe5
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2019-10-27 16:33:48 -0600

    diccionarios

--- Curso_platzi/MyPlayground.playground/Contents.swift
+++ Curso_platzi/MyPlayground.playground/Contents.swift
@@ -29,5 +29,25 @@ transactions.append(40)
 transactions.first
 transactions.last
 transactions.removeFirst()
-print(transactions)
+//print(transactions)
 
+var dailytransactionsDict: [String:[Float]] = [
+    "1nov":[20,10,100.0],
+    "2nov":[1],
+    "3nov":[100],
+    "4nov":[1],
+    "5nov":[111]
+]
+dailytransactionsDict.updateValue([500], forKey: "6nov")
+dailytransactionsDict.sorted { ((key: "1nov", value: [1]), (key: "6nov", value: [1000])) -> Bool in
+    <#code#>
+}
+
+print(dailytransactionsDict)
+//dailytransaction.first
+//dailytransaction[0].isEmpty
+//dailytransaction.append("5nov":[111])
+//print(dailytransactionsDict["5nov"])
+//var dict = [ 1 : "abc", 2 : "cde"]
+//dict.updateValue("efg", forKey: 3)
+//print(dict)
